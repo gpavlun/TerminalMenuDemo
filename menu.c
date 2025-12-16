e21d6724161b32c2592907761c42b6ab5150f30a
Hestr <gregorypavlunenko@gmail.com>
2025-12-15 21:25:36 -0500

    v2

--- menu.c
+++ menu.c
@@ -6,40 +6,120 @@
 #include <poll.h>
 
 #define NUMOPTIONS 8
-int InputScan(struct termios *terminal);
-void PrintMenu(int select);
+struct OptionHeader{
+    int OptionCount;
+    char Title[64];
+    struct Option *List;
+};
+struct Option{
+    char OptionText[32];
+};
+
+int InputScan(void);
+void PrintMenu(int select, struct OptionHeader *OptionHeader);
+struct OptionHeader *InitializeOptions(FILE *OptionsFile);
+int MainMenu(void);
 
 void main(void){
-    struct termios info;
-    tcgetattr(0, &info);          /* get current terminal attirbutes; 0 is the file descriptor for stdin */
-    info.c_lflag &= ~ICANON;      /* disable canonical mode */
-    info.c_cc[VMIN] = 1;          /* wait until at least one keystroke available */
-    info.c_cc[VTIME] = 0;         /* no timeout */
-    info.c_lflag &= ~ECHO;      /* disable echo*/
-    tcsetattr(0, TCSANOW, &info); /* set immediately */
-
-    //FILE *fp = freopen("output.txt", "w", stdin);
-    int select = 0;
-    int ioscan;
-    while(1){
-        system("clear");
-        PrintMenu(select);
-        ioscan = InputScan(&info);
-        if(ioscan==1&&select>0){
-            select--;
-        }else if(ioscan==2&&select<(NUMOPTIONS-1)){
-            select++;
-        }else if(ioscan==5){
-            printf("option %d selected\n",select+1);
-            sleep(2);
-            int c;
-        }
-    }
-
+    MainMenu();
 }
 
-int InputScan(struct termios *terminal){;
-    
+int MainMenu(void){
+
+    struct termios info;
+    tcgetattr(0, &info);
+    info.c_lflag &= ~ICANON;
+    info.c_lflag &= ~ECHO;
+    info.c_cc[VMIN] = 1;
+    info.c_cc[VTIME] = 0;
+    tcsetattr(0, TCSANOW, &info);
+
+    /*opens data file*/
+    FILE *OptionsFile = fopen("options.txt","r");
+    if(OptionsFile==NULL){
+        printf("options file not found\n");
+        return -1;
+    }
+
+    /*creates a table of the options from the file*/
+    struct OptionHeader *OptionHeader;
+    OptionHeader = InitializeOptions(OptionsFile);
+    fclose(OptionsFile);
+
+    /*input scan loop, gets the inputs and updates menu accordingly*/
+    int SelectedOption = 0;
+    int ioscan = 1;
+    while(ioscan!=0){
+        system("clear");
+        PrintMenu(SelectedOption,OptionHeader);
+        ioscan = InputScan();
+        switch(ioscan){
+        case 1:
+            if(SelectedOption>0) SelectedOption--;
+            break;
+        case 2:
+            if(SelectedOption<((OptionHeader->OptionCount)-2)) SelectedOption++;
+            break;
+        case 5:
+            switch(SelectedOption){
+            case 8:
+                ioscan = 0;
+                printf("goodbye!\n");
+                info.c_lflag |= ICANON;
+                info.c_lflag |= ECHO;
+                tcsetattr(0, TCSANOW, &info);
+                break;
+            default:
+                printf("option %d selected\n",SelectedOption+1);
+                break;
+            }
+            sleep(1);
+            break;
+        }
+    }
+}
+struct OptionHeader *InitializeOptions(FILE *OptionsFile){
+    struct OptionHeader *OptionHeader;
+    OptionHeader = (struct OptionHeader *)malloc(sizeof(struct OptionHeader));
+    OptionHeader->OptionCount = 0;
+    char text[32];
+    int i;
+    if(fgets(OptionHeader->Title,64,OptionsFile)==NULL){
+        strcpy(OptionHeader->Title,"not found");
+    }else{
+        i = 0;
+        while((OptionHeader->Title)[i]!='\n'){
+            i++;
+        }
+        (OptionHeader->Title)[i] = '\0';
+    }
+    fgets(text,32,OptionsFile);
+    if(text!=NULL){
+        struct Option *Option;
+        Option = (struct Option *)malloc(sizeof(struct Option));
+        strcpy(Option->OptionText,text);
+        i = 0;
+        while((Option->OptionText)[i]!='\n'){
+            i++;
+        }
+        (Option->OptionText)[i] = '\0';             
+        OptionHeader->OptionCount++;
+        OptionHeader->List = Option;
+        while(fgets(text,32,OptionsFile)!=NULL){
+            Option = (struct Option *)realloc((void *)Option,sizeof(struct Option) * (OptionHeader->OptionCount+1));
+            strcpy((Option+(OptionHeader->OptionCount))->OptionText,text);
+            i = 0;
+            while(((Option+(OptionHeader->OptionCount))->OptionText)[i]!='\n'){
+                i++;
+            }
+            ((Option+(OptionHeader->OptionCount))->OptionText)[i] = '\0';            
+            OptionHeader->OptionCount++;
+        }
+    }
+    return OptionHeader;
+}
+
+int InputScan(void){;
     /***input queue deletion***/
     struct pollfd pfd;
     pfd.fd = STDIN_FILENO;
@@ -56,7 +136,6 @@ int InputScan(struct termios *terminal){;
         }
     }
     /***input queue deletion***/
-
     int returnvalue = -1;
     char userinput = 0;
     while(returnvalue==-1){ 
@@ -64,6 +143,7 @@ int InputScan(struct termios *terminal){;
             /*enter*/
             returnvalue = 5;
         }else if(userinput==27){
+            /*esc*/
             userinput = getchar();
             if(userinput==91){
                 returnvalue = -1;
@@ -76,7 +156,7 @@ int InputScan(struct termios *terminal){;
                 case 66:
                     /*down*/
                     returnvalue = 2;
-                    break;            
+                    break;           
                 }
             }
         }else{
@@ -86,34 +166,19 @@ int InputScan(struct termios *terminal){;
     return returnvalue;
 }
 
-void PrintMenu(int select){
-        
-    char menustring[] = "Fabionno's Brazzianos, e.t. GRIGORI";
-    printf("%s\n",menustring);
+void PrintMenu(int select, struct OptionHeader *OptionHeader){ 
+    printf("\n<<%s>>\n",OptionHeader->Title);
     int i;
     printf("\noptions\n");
-    for(i=0;i<sizeof(menustring);i++){
-        putc('=',stdout);
-    }
-    char options[NUMOPTIONS][32];
-    strcpy(options[0],"spaghetti and meatballs - $100");
-    strcpy(options[1],"spaghetti and meatsauce - $5");
-    strcpy(options[2],"house salad - $50");
-    strcpy(options[3],"chicken alfredo - $10");
-    strcpy(options[4],"lasagna - $20");
-    strcpy(options[5],"garlic bread - no charge");
-    strcpy(options[6],"carne de asada - $30");
-    strcpy(options[7],"water cup - $3");
-
-    putc('\n',stdout);
-    for(i=0;i<NUMOPTIONS;i++){
+    printf("=======\n");
+    for(i=0;i<(OptionHeader->OptionCount)-1;i++){
         printf("%d)",i+1);
         if(i==select){
             putc('[',stdout);
         }else{
             putc(' ',stdout);
         }
-        printf("%s",options[i]);
+        printf("%s",((OptionHeader->List)+i)->OptionText);
         if(i==select){
             putc(']',stdout);
         }else{
@@ -121,5 +186,4 @@ void PrintMenu(int select){
         }
         putc('\n',stdout);      
     }
-
 }
